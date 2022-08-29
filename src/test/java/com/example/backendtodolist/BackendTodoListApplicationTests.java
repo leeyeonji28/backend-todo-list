e17a80ccf2a16f2a37f355e8cbc3caa6d6e7536c
leeyeonji28 <yeon102800@gmail.com>
2022-08-29 23:43:43 +0900

    setting

---
+++ src/test/java/com/example/backendtodolist/BackendTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendtodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTodoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
